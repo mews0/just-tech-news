22f8ef278670f6824f7204bd9d88941303a87602
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-08-27 08:14:07 -0500

    Update login/signup page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
 const router = require(`express`).Router();
 const sequelize = require(`../config/connection`);
-const { Post, User, Comment } = require(`../models`);
+const { Post, User, Comment, Vote } = require(`../models`);
 
+// get all posts for homepage
 router.get(`/`, (req, res) => {
+  console.log(`======================`);
   Post.findAll({
     attributes: [
       `id`,
@@ -28,7 +30,7 @@ router.get(`/`, (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      // pass a single post object into the homepage template
+
       res.render(`homepage`, { posts });
     })
     .catch(err => {
@@ -38,6 +40,11 @@ router.get(`/`, (req, res) => {
 });
 
 router.get(`/login`, (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect(`/`);
+    return;
+  }
+
   res.render(`login`);
 });
 
